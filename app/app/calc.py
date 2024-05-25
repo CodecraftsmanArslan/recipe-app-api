2e8b4c3064d856da862d75faf814152bb20bff01
This PC <arslanaziz2017@gmail.com>
2024-05-25 15:01:22 +0500

    data are change

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,7 +1,9 @@
-
-
+""" Function to add the data"""
 
 
 def add(x,y):
     "Add x and y"
     return x+y
+
+def sub(x,y):
+    return x-y
--- app/app/test.py
+++ app/app/test.py
@@ -8,3 +8,4 @@ class CalcTests(SimpleTestCase):
     def test_add_number(self):
         res=calc.add(3,2)
         self.assertEqual(res,5)
+        
